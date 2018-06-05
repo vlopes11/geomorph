4b75f71eaf8132c39d5665053e481a108c1c2593
Victor Lopez <vhrlopes@gmail.com>
2018-06-06 01:56:17 +0300

    Basic UTM implementation

--- src/utm.rs
+++ src/utm.rs
@@ -1,3 +1,4 @@
+use std::f64::consts;
 use ParseError;
 use coord::Coord;
 
@@ -19,13 +20,11 @@ use coord::Coord;
 /// ```
 ///
 pub struct Utm {
-    easting: f64,
-    northing: f64,
-    point_scale_factor: f64,
-    meridian_convergence: f64,
-    north: bool,
-    zone: u32,
-    band: char,
+    pub easting: f64,
+    pub northing: f64,
+    pub north: bool,
+    pub zone: i32,
+    pub band: char,
 }
 
 impl Utm {
@@ -36,100 +35,102 @@ impl Utm {
         let lat = coord.lat;
         let lon = coord.lon;
 
-        let north = (lat >= 0.0);
-        let zone: u32 = ((lon + 186.0) / 6.0).floor() as u32;
-        let lambda_0: f64 = (zone as f64) * 6.0 - 183.0;
-        let n0: f64;
+        let north: bool = lat >= 0.0;
+        let band: char;
+        if lat < -72.0 {band = 'C';}
+        else if lat < -64.0 {band = 'D';}
+        else if lat < -56.0 {band = 'E';}
+        else if lat < -48.0 {band = 'F';}
+        else if lat < -40.0 {band = 'G';}
+        else if lat < -32.0 {band = 'H';}
+        else if lat < -24.0 {band = 'J';}
+        else if lat < -16.0 {band = 'K';}
+        else if lat < -8.0 {band = 'L';}
+        else if lat < 0.0 {band = 'M';}
+        else if lat < 8.0 {band = 'N';}
+        else if lat < 16.0 {band = 'P';}
+        else if lat < 24.0 {band = 'Q';}
+        else if lat < 32.0 {band = 'R';}
+        else if lat < 40.0 {band = 'S';}
+        else if lat < 48.0 {band = 'T';}
+        else if lat < 56.0 {band = 'U';}
+        else if lat < 64.0 {band = 'V';}
+        else if lat < 72.0 {band = 'W';}
+        else {band = 'X';}
+        
+        let sa: f64 = 6378137.0;
+        let sb: f64 = 6356752.314245;
+        let k_0: f64 = 0.9996;
+        let e_0: f64 = 500000.0;
+        let n_0: f64 = 10000000.0;
+        
+        let e2: f64 = (sa.powi(2) - sb.powi(2)).powf(0.5) / sb;
+        let e2_2: f64 = e2.powi(2);
+        let c: f64 = sa.powi(2) / sb;
+        
+        let rlat: f64 = lat.to_radians();
+        let rlon: f64 = lon.to_radians();
+        
+        let mut zone: i32 = ((lon / 6.0).floor() as i32) + 31;
+        
+        //Treat zone exceptions
+        {
+            let fmod_lon = lon - 360.0 * (lon / 360.0).trunc();
+            let floor_lon = fmod_lon.floor();
+            let ilon: f64;
+            if floor_lon >= 180.0 {ilon = floor_lon - 360.0;}
+            else if floor_lon < -180.0 {ilon = floor_lon + 360.0;}
+            else {ilon = floor_lon;}
+
+            let except_band = 
+                ((lat.floor() + 80.0) / 8.0 - 10.0)
+                .min(9.0)
+                .max(-10.0)
+                .trunc();
+            if except_band == 7.0 && zone == 31 && ilon >= 3.0 {
+                zone = 32;    // Norway UTM exception
+            } else if except_band == 9.0 && ilon >= 0.0 && ilon < 42.0 {
+                // Svalbard UTM exception
+                zone =
+                    (2.0 * ((ilon + 183.0) / 12.0).trunc() + 1.0) as i32;
+            }
+        }
+
+        let delta_s: f64 =
+            rlon - consts::PI * ((zone * 6 - 183) as f64) / 180.0;
+        
+        let a: f64 = rlat.cos() * delta_s.sin();
+        let epsilon: f64 = 0.5 * ((1.0 + a) / (1.0 - a)).ln();
+        let nu: f64 = (rlat.tan() / delta_s.cos()).atan() - rlat;
+        
+        let v: f64 =
+            c * k_0 / (1.0 + e2_2 * rlat.cos().powi(2)).sqrt();
+        let ta: f64 =
+            e2_2 * epsilon.powi(2) * rlat.cos().powi(2) / 2.0;
+        let a1: f64 = (2.0 * rlat).sin();
+        let a2: f64 = a1 * rlat.cos().powi(2);
+        let j2: f64 = rlat + a1 / 2.0;
+        let j4: f64 = (3.0 * j2 + a2) / 4.0;
+        let j6: f64 = (5.0 * j4 + a2 * rlat.cos().powi(2)) / 3.0;
+        let alfa: f64 = 3.0 * e2_2 / 4.0;
+        let beta: f64 = 5.0 * alfa.powi(2) / 3.0;
+        let gama: f64 = 35.0 * alfa.powi(3) / 27.0;
+        let bm: f64 =
+            k_0 * c * (rlat - alfa * j2 + beta * j4 - gama * j6);
+        let easting: f64 = epsilon * v * (1.0 + ta / 3.0) + e_0;
+        let northing: f64;
         if north {
-            n0 = 0.0;
+            northing = nu * v * (1.0 + ta) + bm;
         } else {
-            n0 = 10000.0;
+            northing = nu * v * (1.0 + ta) + bm + n_0;
         }
-
-        // Equatorial radius (a): 63781.37 km
-        // Inverse flattening (1/f): 298.257223563
-        // Polar radius (b): 63567.523142 km
-        // 
-        // first flattening (f) = (a - b) / a
-        // second flattening (f') = (a - b) / b
-        // n = f / (2 - f')
-        let a: f64 = 63781.37;
-        let n: f64 = 0.0016792298759879202;
-		let n_sqrt: f64 = n.sqrt();
-		let a_polynomial: f64 = 6367.449085550872;
-        
-        // Auxiliary variables
-        let t: f64 = (
-                lat.sin().atanh() - (
-                    (2.0 * n_sqrt / (1.0 + n)) * (
-                        (2.0 * n_sqrt / (1.0 + n)) * lat.sin()
-                    )
-                )
-            ).sinh();
-        let epsilon: f64 = (t / (lon - lambda_0).cos()).atan();
-        let nl = ((lon - lambda_0).sin() / (1.0 + t * t).sqrt()).atanh();
-
-        let alpha = vec![
-            1.0 * n / 2.0 - 2.0 * n.powi(2) / 3.0 + 5.0 * n.powi(3) / 16.0,
-            13.0 * n.powi(2) / 48.0 - 3.0 * n.powi(3) / 5.0,
-            61.0 * n.powi(3) / 240.0
-        ];
-        let beta = vec![
-            1.0 * n / 2.0 - 2.0 * n.powi(2) / 3.0 + 37.0 * n.powi(3) / 96.0,
-            1.0 * n.powi(2) / 48.0 + 1.0 * n.powi(3) / 15.0,
-            17.0 * n.powi(3) / 480.0
-        ];
-        let delta = vec![
-            2.0 * n - 2.0 * n.powi(2) / 3.0 - 2.0 * n.powi(3),
-            7.0 * n.powi(2) / 3.0 - 8.0 * n.powi(3) / 5.0,
-            56.0 * n.powi(3) / 15.0
-        ];
-
-        let mut av: f64 = 1.0;
-        for j in 1..3 {
-            let fj: f64 = j as f64;
-            av += 2.0 * fj * alpha[j] * (2.0 * fj * epsilon).cos() * (2.0 * fj * nl).cosh();
-        }
-
-        let mut tao: f64 = 0.0;
-        for j in 1..3 {
-            let fj: f64 = j as f64;
-            tao += 2.0 * fj * alpha[j] * (2.0 * fj * epsilon).sin() * (2.0 * fj * nl).sinh();
-        }
-        
-        // Convention
-        let e0: f64 = 500.0;
-        let k0: f64 = 0.9996;
-
-        let mut easting: f64 = 0.0;
-        for j in 1..3 {
-            let fj: f64 = j as f64;
-            easting += alpha[j] * (2.0 * fj * epsilon).cos() * (2.0 * fj * nl).sinh();
-        }
-        easting = e0 + k0 * a_polynomial * (nl + easting);
-
-        let mut northing: f64 = 0.0;
-        for j in 1..3 {
-            let fj: f64 = j as f64;
-            northing += alpha[j] * (2.0 * fj * epsilon).sin() * (2.0 * fj * nl).cosh();
-        }
-        northing = n0 + k0 * a_polynomial * (epsilon + northing);
-
-        let point_scale_factor: f64 = 
-            (k0 * a_polynomial / a) * 
-            ((1.0 + (((1.0 - n) / (1.0 + n)) * lat.tan()).powi(2)) * (av.powi(2) + tao.powi(2)) / (t.powi(2) + ((lon - lambda_0).cos()).powi(2))).sqrt();
-
-        let meridian_convergence: f64 = 
-            ((tao * (1.0 + t.powi(2)).sqrt() + (av * t * (lon - lambda_0).tan())) / (av * (1.0 + t.powi(2)).sqrt() - tao * t * (lon - lambda_0).tan())).atan();
 
         Ok(Utm {
             easting,
             northing,
-            point_scale_factor,
-            meridian_convergence,
             north,
             zone,
-            band: 'A',
+            band,
         })
     }
 }
@@ -142,44 +143,76 @@ mod tests {
     fn utm_zone_south() {
         let coord = Coord {lat: -23.0095839, lon: -43.4361816};
         let utm = Utm::new(coord).unwrap();
-        assert_eq!(utm.easting, 23.0);
-        assert_eq!(utm.northing, 24.0);
-        assert_eq!(utm.point_scale_factor, 25.0);
-        assert_eq!(utm.meridian_convergence, 63.0);
-        assert_eq!(utm.north, true);
+        assert!((utm.easting - 660265.0944068021).abs() < 1.0);
+        assert!((utm.northing - 7454564.243324452).abs() < 1.0);
+        assert_eq!(utm.north, false);
         assert_eq!(utm.zone, 23);
         assert_eq!(utm.band, 'K');
     }
-    /*
+
+    #[test]
+    fn utm_zone_north() {
+        let coord = Coord {lat: 52.517153, lon: 13.412389};
+        let utm = Utm::new(coord).unwrap();
+        assert!((utm.easting - 392273.6051633584).abs() < 1.0);
+        assert!((utm.northing - 5819744.4599129185).abs() < 1.0);
+        assert_eq!(utm.north, true);
+        assert_eq!(utm.zone, 33);
+        assert_eq!(utm.band, 'U');
+    }
+
     #[test]
     fn utm_norway_zone() {
         let coord = Coord {lat: 61.076521, lon: 4.680180};
         let utm = Utm::new(coord).unwrap();
+        assert!((utm.easting - 267038.76).abs() < 1.0);
+        assert!((utm.northing - 6779002.66).abs() < 1.0);
+        assert_eq!(utm.north, true);
         assert_eq!(utm.zone, 32);
         assert_eq!(utm.band, 'V');
     }
-
+    
     #[test]
-    fn utm_svalbard_zone() {
+    fn utm_svalbard_zone_1() {
         let coord = Coord {lat: 78.891608, lon: 10.457194};
         let utm = Utm::new(coord).unwrap();
+        assert!((utm.easting - 402386.73).abs() < 1.0);
+        assert!((utm.northing - 8761675.98).abs() < 1.0);
+        assert_eq!(utm.north, true);
         assert_eq!(utm.zone, 33);
         assert_eq!(utm.band, 'X');
+    }
 
+    #[test]
+    fn utm_svalbard_zone_2() {
         let coord = Coord {lat: 78.122200, lon: 20.349504};
         let utm = Utm::new(coord).unwrap();
+        assert!((utm.easting - 622751.81).abs() < 1.0);
+        assert!((utm.northing - 8677619.41).abs() < 1.0);
+        assert_eq!(utm.north, true);
         assert_eq!(utm.zone, 33);
         assert_eq!(utm.band, 'X');
+    }
 
+    #[test]
+    fn utm_svalbard_zone_3() {
         let coord = Coord {lat: 78.102575, lon: 21.013745};
         let utm = Utm::new(coord).unwrap();
-        assert_eq!(utm.zone, 35);
-        assert_eq!(utm.band, 'X');
-
-        let coord = Coord {lat: 78.138264, lon: 30.194746};
-        let utm = Utm::new(coord).unwrap();
+        assert!((utm.easting - 362459.56).abs() < 1.0);
+        assert!((utm.northing - 8676854.75).abs() < 1.0);
+        assert_eq!(utm.north, true);
         assert_eq!(utm.zone, 35);
         assert_eq!(utm.band, 'X');
     }
-    */
+
+    #[test]
+    fn utm_svalbard_zone_4() {
+        let coord = Coord {lat: 78.138264, lon: 30.194746};
+        let utm = Utm::new(coord).unwrap();
+        assert!((utm.easting - 573272.89).abs() < 1.0);
+        assert!((utm.northing - 8675799.74).abs() < 1.0);
+        assert_eq!(utm.north, true);
+        assert_eq!(utm.zone, 35);
+        assert_eq!(utm.band, 'X');
+    }
 }
